38fa8a5de36682ef54d21b647376be52934f779d
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-02-16 21:23:29 -0500

    header.js cide edits

---
+++ loltabs/src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
+
+class Header extends Component {
+  render() {
+    return 
+  }
+}
+
+export default Header;
