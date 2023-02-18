0a5b35cc385e157101ed8c6699dee15b3ad1c27d
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-02-17 21:01:36 -0500

    app.js /css updates

--- loltabs/src/components/App.js
+++ loltabs/src/components/App.js
@@ -1,1 +1,39 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./Home.js";
+import Popup from "./Popup.js";
 
+
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Route
+            exact={true}
+            path="/"
+            render={() => {
+              return <Home />;
+            }}
+          />
+          <Route
+            exact={true}
+            path="/popup"
+            render={() => {
+              return <Popup />;
+            }}
+          />
+
+
+          </div>
+      </BrowserRouter>
+    );
+  }
+
+
+}
+  
+
+  
+export default App;
