1467747849b66f499a5ef84d98ab27db628e0555
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-02-17 20:57:45 -0500

    popup.js edit

--- loltabs/src/components/Popup.js
+++ loltabs/src/components/Popup.js
@@ -4,8 +4,14 @@ import "./App.css";
 class Popup extends Component {
   render() {
     return (
-  
-    );
+      <div>
+      <div className="floatRight">
+      <img onClick={this.props.closePopup} className="closePopup" src ="close-btn.png" alt="close-popup"/>
+      </div>
+      <img className="centerIMG" src={this.props.image} alt="" />
+    </div>
+  );
+    
   }
 }
 
